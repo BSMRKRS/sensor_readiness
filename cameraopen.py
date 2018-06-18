9daa58ba970c3219752321f099e5c3ec09be27cd
ekrueger19 <33067215+ekrueger19@users.noreply.github.com>
2018-06-18 17:19:01 -0500

    This should open the cameras on the robot

---
+++ cameraopen.py
@@ -0,0 +1,13 @@
+import setup
+import RoboPiLib as RPL
+import time
+import webbrowser
+
+print "Open front camera, back camera, or both. Enter 'front', 'back', or 'both'"
+if raw_input("> ") == "front":
+    webbrowser.open('http://192.168.21.181/top.htm') #opens front camera
+if raw_input("> ") == "back":
+    webbrowser.open('http://192.168.21.182/top.htm') #opens back camera
+elif raw_input("> ") == "both":
+    webbrowser.open('http://192.168.21.181/top.htm')
+    webbrowser.open('http://192.168.21.182/top.htm')
