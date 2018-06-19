bfd80fb68531e8f75d2c4bf2f448ec3493f1399f
ekrueger19 <33067215+ekrueger19@users.noreply.github.com>
2018-06-19 00:23:32 -0400

    I GOT IT!!! YES!!! FINALLY!!! IN YOUR FACE PYTHON!!!

--- controllers/cameraturn.py
+++ controllers/cameraturn.py
@@ -1,27 +1,30 @@
+#You have to enter Left or Right twice for it to turn.
+#Also, it's caps-sensitive
 import setup
 import RoboPiLib as RPL
 import time as time
 
 #motor pin
-mv = 8
+mv = 7
 
 #speed
-left = 100
-right = 1000
+left = 1000
+right = 2000
 
 print "Left or Right?"
 
-while raw_input("> ") == "Left":
-    bro = time.time() + 0.5
-    while time.time() < bro:
-        RPL.servoWrite(mv, left)
-        if time.time() >= bro:
-            RPL.servoWrite(mv, 0)
-while raw_input("> ") == "Right":
-    bruh = time.time() + 0.5
-    while time.time() < bruh:
-        RPL.servoWrite(mv, right)
-        if time.time() >= bruh:
-            RPL.servoWrite(mv, 0)
-else:
-    print "Made a mistake there, bud."
+while True:
+    while raw_input("> ") == "Left":
+        bro = time.time() + 0.5
+        while time.time() < bro:
+            RPL.servoWrite(mv, left)
+            if time.time() >= bro:
+                RPL.servoWrite(mv, 0)
+    while raw_input("> ") == "Right":
+        bruh = time.time() + 0.5
+        while time.time() < bruh:
+            RPL.servoWrite(mv, right)
+            if time.time() >= bruh:
+                RPL.servoWrite(mv, 0)
+    else:
+        print "Made a mistake there, bud."
