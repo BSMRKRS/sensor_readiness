acebf0bdf388979f32cc9d31ff3972c73acdf89e
ekrueger19 <33067215+ekrueger19@users.noreply.github.com>
2018-06-18 20:41:13 -0500

    Both wasn't working before, but I fixed it

--- cameraopen.py
+++ cameraopen.py
@@ -1,10 +1,8 @@
 import webbrowser
 
-print "Open front camera, back camera, or both. Enter 'front', 'back', or 'both'"
-if raw_input("> ") == "front":
-    webbrowser.open('http://192.168.21.181/top.htm') #opens front camera
-if raw_input("> ") == "back":
-    webbrowser.open('http://192.168.21.182/top.htm') #opens back camera
-if raw_input("> ") == "both":
+print "Open both cameras. Enter 'both'"
+if raw_input("> ") == "both": #opens both cameras
     webbrowser.open('http://192.168.21.181/top.htm')
     webbrowser.open('http://192.168.21.182/top.htm')
+else:
+    print "Bruh you messed up try again"
